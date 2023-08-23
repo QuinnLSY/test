54090b11dc62aba7778e8691cac6162eef30fee5
QuinnLSY <137391474+QuinnLSY@users.noreply.github.com>
2023-08-23 22:59:23 +0800

    Add files via upload

--- SUPER_MARIO/source/components/player.py
+++ SUPER_MARIO/source/components/player.py
@@ -9,6 +9,82 @@ import pygame
 from SUPER_MARIO.source.setup import *
 from SUPER_MARIO.source.tools import *
 
-class Player(pygame.sprite.Sprite):
+
+class Player(pygame.sprite.Sprite):  # 在level中调用
     def __init__(self, name):
-        
+        pygame.sprite.Sprite.__init__(self)
+        self.name = name
+        self.setup_states()
+        self.setup_velocities()
+        self.setup_timers()
+        self.load_images()
+
+    # 设置状态
+    def setup_states(self):
+        self.face_right = True
+        self.dead = False
+        self.big = False
+
+    # 各种速度
+    def setup_velocities(self):
+        self.x_vel = 0
+        self.y_vel = 0
+
+    # 各状态计时器
+    def setup_timers(self):
+        self.walking_timer = 0
+        self.transition_timer = 0
+
+    def load_images(self):
+        sheet = GRAPHICS['mario_bros']
+        self.right_frames = []
+        self.left_frames = []
+        self.up_frames = []
+        self.down_frames = []
+
+        frame_rects = [
+            (178, 32, 12, 16),
+            (80, 32, 15, 16),
+            (96, 32, 16, 16),
+            (112, 32, 16, 16)
+        ]
+
+        for frame_rect in frame_rects:
+            right_image = get_image(sheet, *frame_rect, (0, 0, 0), PLAYER_MULTI)
+            left_image = pygame.transform.flip(right_image, True, False)  # 镜像翻转
+            up_image = pygame.transform.rotate(right_image, 90)
+            down_image = pygame.transform.rotate(right_image, -90)
+            self.right_frames.append(right_image)
+            self.left_frames.append(left_image)
+            self.up_frames.append(up_image)
+            self.down_frames.append(down_image)
+
+        self.frame_index = 0
+        self.frames = self.right_frames
+        self.image = self.frames[self.frame_index]
+        self.rect = self.image.get_rect()
+
+    def update(self, keys):
+        self.current_time = pygame.time.get_ticks()
+        if keys[pygame.K_RIGHT]:
+            self.x_vel = 5
+            self.y_vel = 0
+            self.frames = self.right_frames
+        if keys[pygame.K_LEFT]:
+            self.x_vel = -5
+            self.y_vel = 0
+            self.frames = self.left_frames
+        if keys[pygame.K_UP]:
+            self.x_vel = 0
+            self.y_vel = -5
+            self.frames = self.up_frames
+        if keys[pygame.K_DOWN]:
+            self.x_vel = 0
+            self.y_vel = 5
+            self.frames = self.down_frames
+        # 帧动画
+        if self.current_time - self.walking_timer > 100:
+            self.walking_timer = self.current_time
+            self.frame_index += 1
+            self.frame_index %= 4
+        self.image = self.frames[self.frame_index]
--- SUPER_MARIO/source/states/level.py
+++ SUPER_MARIO/source/states/level.py
@@ -7,6 +7,7 @@
 """
 import pygame
 from SUPER_MARIO.source.states.main_menu import *
+from SUPER_MARIO.source.components.player import *
 
 
 class Level:
@@ -15,6 +16,12 @@ class Level:
         self.next = None
         self.info = Info('level')
         self.setup_background()
+        self.setup_player()
+
+    def setup_player(self):
+        self.player = Player('mario')
+        self.player.rect.x = 300
+        self.player.rect.y = 300
 
     def setup_background(self):
         self.background = GRAPHICS['level_1']
@@ -24,9 +31,16 @@ class Level:
         self.background_rect = self.background.get_rect()
 
     def update(self, surface, keys):
+        self.player.update(keys)
+        self.update_player_position()
         self.draw(surface)
+
+    def update_player_position(self):
+        self.player.rect.x += self.player.x_vel
+        self.player.rect.y += self.player.y_vel
 
     def draw(self, surface):
         surface.blit(self.background, (0, 0))
+        surface.blit(self.player.image, self.player.rect)
         self.info.update()  # 金币闪烁
         self.info.draw(surface)
